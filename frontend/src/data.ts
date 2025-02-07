3d06b7e097087bd64a874c4fd0a86ba762c5f712
nour427 <nour.chaaben@insat.ucar.tn>
2025-02-07 20:29:04 +0100

    added data examples

---
+++ frontend/src/data.ts
@@ -0,0 +1,31 @@
+import { Options } from './app/shared/models/Options';
+
+export const sample_options: Options[] = [
+  {
+    id:'1',
+    name: 'Fritte',
+    price: 10,
+    selected:true
+  },
+  {
+    id:'2',
+    name: 'Bacon',
+    price: 20.00,
+    selected:true
+
+  },
+  {
+    id:'3',
+    name: 'Boisson',
+    price: 5,
+    selected:true
+  },
+  {
+    id:'4',
+    name: 'Dessert',
+    price: 2.00,
+    selected:true
+  }
+ 
+]
+
